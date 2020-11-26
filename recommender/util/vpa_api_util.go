604b558cb4109c1f71693d7438e859fe1ec0cc89
tuxudong <I1>
2020-11-26 19:05:28 +0800

    recommender init 3

--- recommender/util/vpa_api_util.go
+++ recommender/util/vpa_api_util.go
@@ -8,6 +8,7 @@ import (
 func GetContainerResourcePolicy(containerName string, policy *vpa_types.PodResourcePolicy) *vpa_types.ContainerResourcePolicy {
 	return nil
 }
+
 func PodLabelsMatchVPA(podNameSpace string, labels labels.Set, vpaNamespace string, vpaSelector labels.Selector) bool {
 	if podNameSpace != vpaNamespace {
 		return false

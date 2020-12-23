fb102ccdf009a590cfdb5d853bd8ad2d3b8cfc6e
tuxudong <I1>
2020-12-23 19:32:06 +0800

    recommender init 9

--- experiments/autopilot/main.go
+++ experiments/autopilot/main.go
@@ -80,9 +80,9 @@ func main() {
 			for _, recon := range recommendation.ContainerRecommendations {
 
 				recommendString := fmt.Sprintf("%s,%s,%s,%f",
-					recon.Target.Cpu().String(),
-					recon.UpperBound.Cpu().String(),
-					recon.LowerBound.Cpu().String(), d)
+					recon.Target.Cpu().AsDec().String(),
+					recon.UpperBound.Cpu().AsDec().String(),
+					recon.LowerBound.Cpu().AsDec().String(), d)
 				klog.Info(recommendString)
 			}
 		}

14c5d7698a731eedef2d3ced6d5f2054ed2cb8ca
tuxudong <I1>
2020-12-24 16:01:18 +0800

    recommender init 10

--- recommender/auto/autopilot.go
+++ recommender/auto/autopilot.go
@@ -136,9 +136,9 @@ func (r *recommender) RunOnce(element, csv string) {
 	for _, s := range entityAggregateStateMap {
 		timestamp := anyTime
 		for i := 0; i < len(data); i++ {
-			d := data[i][1]
+			d := data[i][1] / 100
 			s.AddSample(&model.ContainerUsageSample{
-				timestamp, model.CPUAmountFromCores(d / 100), model.CPUAmountFromCores(1), model.ResourceCPU})
+				timestamp, model.CPUAmountFromCores(d), model.CPUAmountFromCores(1), model.ResourceCPU})
 
 			timestamp = timestamp.Add(time.Duration(*sampleSecondInterval) * time.Second)
 

1e85f45289a6df21da4fd58d6216cb8e83742c73
tuxudong <I1>
2020-12-24 15:02:53 +0800

    recommender init 9

--- experiments/auto/main.go
+++ experiments/auto/main.go
@@ -9,7 +9,7 @@ import (
 
 // Aggregation configuration flags
 var (
-	elementId                      = flag.String("element-id", "unknow", `element id for resource usage prediction.`)
+	elementId                      = flag.String("element-id", "unknow", `资源利用率预测的实例ID`)
 	csvFile                        = flag.String("csv-file", "unkowfile", `metrics data file from monitor system, csv file`)
 	memoryAggregationInterval      = flag.Duration("memory-aggregation-interval", model.DefaultMemoryAggregationInterval, `The length of a single interval, for which the peak memory usage is computed. Memory usage peaks are aggregated in multiples of this interval. In other words there is one memory usage sample per interval (the maximum usage over that interval)`)
 	memoryAggregationIntervalCount = flag.Int64("memory-aggregation-interval-count", model.DefaultMemoryAggregationIntervalCount, `The number of consecutive memory-aggregation-intervals which make up the MemoryAggregationWindowLength which in turn is the period for memory usage aggregation by VPA. In other words, MemoryAggregationWindowLength = memory-aggregation-interval * memory-aggregation-interval-count.`)

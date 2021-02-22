645b2eb0fe29584944480192505d87a3b1d7c949
tuxudong <I1>
2021-02-22 14:39:12 +0800

    v1 adapter for general scheduler debug- sets replaced

--- scheduler/model/types_deepcopy.go
+++ scheduler/model/types_deepcopy.go
@@ -2,12 +2,36 @@
 //
 package model
 
-func (in *Pod) DeepCopy() *Pod {
-	return nil
-}
+import (
+	"k8s.io/apimachinery/pkg/runtime"
+)
+
 func (in *Node) DeepCopy() *Node {
 	return nil
 }
 func (in *TopologySpreadConstraint) DeepCopyInto(out *TopologySpreadConstraint) {
 
+}
+
+func (in *Pod) DeepCopyInto(out *Pod) {
+	*out = *in
+	out.TypeMeta = in.TypeMeta
+	return
+}
+
+func (in *Pod) DeepCopy() (out *Pod) {
+	if in == nil {
+		return nil
+	}
+	out = new(Pod)
+	in.DeepCopyInto(out)
+	return out
+}
+
+func (in *Pod) DeepCopyObject() runtime.Object {
+
+	if c := in.DeepCopy(); c != nil {
+		return c
+	}
+	return nil
 }

10004f289b7b0606342243a04816da12b784e138
tuxudong <I1>
2021-02-26 17:52:00 +0800

    v1 adapter for general scheduler debug - debugging

--- scheduler/apis/config/scheme/scheme.go
+++ scheduler/apis/config/scheme/scheme.go
@@ -7,6 +7,7 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 )
 
+// 插件增加，对应的产检参数需要这里集中注册，后续改成通过反射来实现
 func NewFromSchemeByName(name string) runtime.Object {
 	switch {
 	case name == "PodTopologySpread":

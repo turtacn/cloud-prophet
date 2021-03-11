b1ff16486883620f2bf6163244f9792f0c9cea5b
tuxudong <I1>
2021-03-11 20:21:05 +0800

    v2.0 scheduler estimator

--- experiments/scheduler/main.go
+++ experiments/scheduler/main.go
@@ -44,7 +44,7 @@ func main() {
 	}
 
 	// Load host info csv file
-	for round := 0; round < 2; round++ {
+	for round := 0; round < 3; round++ {
 		for i, h := range test.LoadHostInfo(*hostInfoFile) {
 			if i == 0 {
 				continue
